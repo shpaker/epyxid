efd3b09492c97bfe2758cb5910d655193c9f3a7b
A.Shpak <aleksander.shpak.als@gmail.com>
2026-08-27 07:54:57 +0300

    build: bump to 0.3.6, pyo3 0.29, abi3 wheels, commit Cargo.lock

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,6 @@ use crate::errors::XIDError;
 use crate::utils::{xid_create, xid_from_bytes, xid_from_str};
 use crate::wrapper::XID;
 
-const PY_MODULE_VERSION: &str = "0.3.5";
-
 mod errors;
 mod utils;
 mod wrapper;
@@ -19,6 +17,6 @@ fn epyxid(py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(xid_from_str, m)?)?;
     m.add_function(wrap_pyfunction!(xid_from_bytes, m)?)?;
     m.add("XIDError", py.get_type::<XIDError>())?;
-    m.add("__version__", PY_MODULE_VERSION)?;
+    m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     Ok(())
 }
